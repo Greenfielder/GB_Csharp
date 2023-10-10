8c0a4c3395710b52048a94b18fbeb3f035da25c1
Savenko Dmitry <dimon-sad@mail.ru>
2023-10-10 20:57:05 +0300

    Home work 6

---
+++ lesson6/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int readNum(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void inArray(int lenght)
+{
+    int count = 0;
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = readNum("Введите число");
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine("Количество чисел больше 0: " + count);
+}
+
+int lenght = readNum("Введите количиство вводимых чисел");
+inArray(lenght);
---
+++ lesson6/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+string[] input()
+{
+    Console.WriteLine("Введите значения b1, k1, b2 и k2 через пробел:");
+    string[] coordinates = Console.ReadLine().Split(' ');
+    return coordinates;
+}
+
+void validator(string[] coordinates)
+{
+    double b1 = double.Parse(coordinates[0]);
+    double k1 = double.Parse(coordinates[1]);
+    double b2 = double.Parse(coordinates[2]);
+    double k2 = double.Parse(coordinates[3]);
+    if (k1 == k2)
+    {
+        Console.WriteLine("Прямые параллельны, нет точки пересечения.");
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
+    }
+}
+
+string[] coordinates = input();
+validator(coordinates);
